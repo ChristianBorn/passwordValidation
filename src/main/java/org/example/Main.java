f47c47b94a0f8cefb85e101ef0d4d4d3c66b3e79
christianborn <chrisborn@hotmail.de>
2022-09-30 10:26:11 +0200

    Add check for password length (min 8, max 100 characters)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static boolean passwordInput(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
+        else return password.length() <= 100;
+    }
 }
